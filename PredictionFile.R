c3209e68544d83d4382be3b65e0a121956fd2686
Julian Ariza <53215300+SrAriza@users.noreply.github.com>
2019-09-16 13:42:23 +0200

    Update PredictionFile.R

--- PredictionFile.R
+++ PredictionFile.R
@@ -11,5 +11,16 @@ library(caret)
 library(ggplot2)
 library(dplyr)
 
-
+#Create Cluster with desired number of cores.  
 cl <- makeCluster(3)
+registerDoParallel(cl)
+
+# Register Cluster
+registerDoParallel(cl)
+
+# Confirm how many cores are now "assigned" to R and RStudio
+getDoParWorkers() 
+
+# Stop Cluster. After performing your tasks, stop your cluster. 
+stopCluster(cl)
+
